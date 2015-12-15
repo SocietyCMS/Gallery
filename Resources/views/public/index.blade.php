4ab7a87de9dcc356883539ea0dcb0bbc377549a6
Ralph Huwiler <ralph@huwiler.rocks>
2015-12-15 09:31:58 +0100

    fix crash bug on empty album

--- Resources/views/public/index.blade.php
+++ Resources/views/public/index.blade.php
@@ -20,10 +20,12 @@
 
             <div class="row">
                 @foreach ($albums as $album)
-                    <div class="col-xs-6 col-md-3">
-                        <a href="{{route('gallery.show', $album->slug)}}"><img src="{{ $album->cover->first()->getFirstMediaUrl('images', 'wide320')}}" class="img-thumbnail" alt="{{$album->title}}"></a>
-                        <h3><a href="{{route('gallery.show', $album->slug)}}">{{$album->title}}</a></h3>
-                    </div>
+                    @if($album->photos->count() > 0)
+                        <div class="col-xs-6 col-md-3">
+                            <a href="{{route('gallery.show', $album->slug)}}"><img src="{{ $album->cover->first()->getFirstMediaUrl('images', 'wide320')}}" class="img-thumbnail" alt="{{$album->title}}"></a>
+                            <h3><a href="{{route('gallery.show', $album->slug)}}">{{$album->title}}</a></h3>
+                        </div>
+                    @endif
                 @endforeach
 
             </div>
