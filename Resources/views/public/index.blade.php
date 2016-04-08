f4b524ea7835797ddf325fdd24ebbb3c86002289
Ralph Huwiler <ralph@huwiler.rocks>
2016-04-08 18:17:48 +0200

    update to bootstrap 4

--- Resources/views/public/index.blade.php
+++ Resources/views/public/index.blade.php
@@ -9,9 +9,14 @@
     <div class="row">
         @foreach ($albums as $album)
             @if($album->photos->count() > 0)
-                <div class="col-xs-6 col-md-3">
-                    <a href="{{route('gallery.show', $album->slug)}}"><img src="{{ $album->cover->first()->getFirstMediaUrl('images', 'wide320')}}" class="img-thumbnail" alt="{{$album->title}}"></a>
-                    <h3><a href="{{route('gallery.show', $album->slug)}}">{{$album->title}}</a></h3>
+
+                <div class="col-sm-4">
+                    <a href="{{route('gallery.show', $album->slug)}}">
+                        <img src="{{ $album->cover->first()->getFirstMediaUrl('images', 'wide320')}}" class="img-fluid img-rounded" alt="{{$album->title}}">
+                    </a>
+                    <h1>
+                        <a href="{{route('gallery.show', $album->slug)}}">{{$album->title}}</a>
+                    </h1>
                 </div>
             @endif
         @endforeach
