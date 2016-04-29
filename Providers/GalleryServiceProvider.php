7a319230437fd8c4a948244da0f58ef56cc5edee
Ralph Huwiler <ralph@huwiler.rocks>
2016-04-29 18:58:53 +0200

    remove duplicate functionality

--- Providers/GalleryServiceProvider.php
+++ Providers/GalleryServiceProvider.php
@@ -20,9 +20,6 @@ class GalleryServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->registerConfig();
-        $this->registerTranslations();
-        $this->registerViews();
     }
 
     /**
@@ -32,55 +29,6 @@ class GalleryServiceProvider extends ServiceProvider
      */
     public function register()
     {
-    }
-
-    /**
-     * Register config.
-     *
-     * @return void
-     */
-    protected function registerConfig()
-    {
-        $this->publishes([
-            __DIR__.'/../Config/config.php' => config_path('gallery.php'),
-        ]);
-        $this->mergeConfigFrom(
-            __DIR__.'/../Config/config.php', 'gallery'
-        );
-    }
-
-    /**
-     * Register views.
-     *
-     * @return void
-     */
-    public function registerViews()
-    {
-        $viewPath = base_path('resources/views/modules/gallery');
-
-        $sourcePath = __DIR__.'/../Resources/views';
-
-        $this->publishes([
-            $sourcePath => $viewPath,
-        ]);
-
-        $this->loadViewsFrom([$viewPath, $sourcePath], 'gallery');
-    }
-
-    /**
-     * Register translations.
-     *
-     * @return void
-     */
-    public function registerTranslations()
-    {
-        $langPath = base_path('resources/lang/modules/gallery');
-
-        if (is_dir($langPath)) {
-            $this->loadTranslationsFrom($langPath, 'gallery');
-        } else {
-            $this->loadTranslationsFrom(__DIR__.'/../Resources/lang', 'gallery');
-        }
     }
 
     /**
